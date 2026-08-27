4bfeb24616a111215507a2eba2cf8e2be6f7dbcb
DwashKarma <dwaskc@gmail.com>
2026-08-27 14:19:17 +1000

    check horizontal wins

--- Board.cs
+++ Board.cs
@@ -2,94 +2,144 @@ public class Board
 {
 
     private Stone?[,] board;
-    public Board(){
-        board= new Stone?[10,10];
-        
+    public Board()
+    {
+        board = new Stone?[10, 10];
+
     }
 
 
-    public void DisplayBoard(){
+    public void DisplayBoard()
+    {
         Console.WriteLine("    1   2   3   4   5   6   7   8   9   10");
         Console.WriteLine("  +---+---+---+---+---+---+---+---+---+---+");
-        for(int i=1;i<=10;i++){
+        for (int i = 1; i <= 10; i++)
+        {
 
             Console.Write($"{i} ");
-            for (int col=1;col<=10;col++){
-               if(board[i-1,col-1]==null){
-                Console.Write("|   ");
-               }
-               else{
-                Console.Write($"| {board[i-1,col-1].Symbol} ");
-               }
-                
+            for (int col = 1; col <= 10; col++)
+            {
+                if (board[i - 1, col - 1] == null)
+                {
+                    Console.Write("|   ");
                 }
+                else
+                {
+                    Console.Write($"| {board[i - 1, col - 1].Symbol} ");
+                }
+
+            }
             Console.WriteLine("|");
             Console.WriteLine("  +---+---+---+---+---+---+---+---+---+---+");
 
         }
     }
 
-    public bool IsValidCell(int row, int col){
-        if(row>=1 && row<11 && col>=1 && col<11){
+    public bool IsValidCell(int row, int col)
+    {
+        if (row >= 1 && row < 11 && col >= 1 && col < 11)
+        {
             return true;
         }
-        else{
+        else
+        {
             return false;
         }
     }
 
-    public bool IsEmptyCell(int row,int col){
-        if(board[row-1,col-1]==null){
+    public bool IsEmptyCell(int row, int col)
+    {
+        if (board[row - 1, col - 1] == null)
+        {
             return true;
         }
-        else{
+        else
+        {
             return false;
         }
     }
 
-    public bool PlaceStone(int row, int col, Stone stone){
-        if(IsValidCell(row,col) && IsEmptyCell(row,col)){
-        board[row-1,col-1] =stone;
-        return true;
+    public bool PlaceStone(int row, int col, Stone stone)
+    {
+        if (IsValidCell(row, col) && IsEmptyCell(row, col))
+        {
+            board[row - 1, col - 1] = stone;
+            return true;
         }
-        else{
+        else
+        {
             Console.WriteLine($"Cannot place stone at {row},{col}. Cell is either not valid or already assigned.");
             return false;
         }
     }
 
-    public bool EraseStone(int row, int col,PlayerTypes currentPlayer){
+    public bool EraseStone(int row, int col, PlayerTypes currentPlayer)
+    {
 
-        
-        if(!IsValidCell(row,col) ){
+
+        if (!IsValidCell(row, col))
+        {
             Console.WriteLine($"Cannot erase stone at {row},{col}. Cell is either not valid.");
-            return  false;
+            return false;
         }
-        if(IsEmptyCell(row,col)){
+        if (IsEmptyCell(row, col))
+        {
             Console.WriteLine($"Cannot erase stone at {row},{col}. Cell is already empty.");
             return false;
         }
 
 
-        Stone? targetStone = board[row - 1, col - 1];
-            
-        bool IsOpponentStone=targetStone.Player != currentPlayer;
+        Stone targetStone = board[row - 1, col - 1]!;
 
-        bool IsHeavyStone=targetStone.Type==StoneTypes.Heavy;
-        if(IsHeavyStone){
+        bool IsOpponentStone = targetStone.Player != currentPlayer;
+
+        bool IsHeavyStone = targetStone.Type == StoneTypes.Heavy;
+        if (IsHeavyStone)
+        {
             Console.WriteLine($"Cannot erase stone at {row},{col}. Stone is heavy.");
             return false;
         }
 
-        if (IsOpponentStone && !IsHeavyStone){
-            board[row-1,col-1]=null;
+        if (IsOpponentStone && !IsHeavyStone)
+        {
+            board[row - 1, col - 1] = null;
             return true;
         }
-        else{
+        else
+        {
             Console.WriteLine($"Cannot erase stone at {row},{col}. Cell is either not valid or already empty.");
             return false;
         }
-        
 
+
+    }
+
+
+    public bool CheckHorizontal(PlayerTypes player)
+    {
+        for (int row = 0; row < 10; row++)
+        {
+            int count = 0;
+            for (int col = 0; col < 10; col++)
+            {
+                if (board[row, col] != null && board[row, col]!.Player == player)
+                {
+                    count++;
+                    if (count == 5)
+                    {
+                        return true;
+                    }
+
+
+                }
+                else
+                {
+                    count = 0;
+
+                }
+
+            }
+        }
+        return false;
     }
 }
--- Game.cs
+++ Game.cs
@@ -1,101 +1,132 @@
-public class Game{
+public class Game
+{
 
     private Board board;
     private Player player1;
     private Player player2;
     private Player currentPlayer;
 
-    public Game(){
+    public Game()
+    {
         this.board = new Board();
         this.player1 = new Player(PlayerTypes.Player1);
         this.player2 = new Player(PlayerTypes.Player2);
         this.currentPlayer = this.player1;
     }
 
-    private void SwitchPlayer(){
-        if (currentPlayer.PlayerType==PlayerTypes.Player1){
-            currentPlayer=player2;
+    private void SwitchPlayer()
+    {
+        if (currentPlayer.PlayerType == PlayerTypes.Player1)
+        {
+            currentPlayer = player2;
         }
-        else{
-            currentPlayer=player1;
+        else
+        {
+            currentPlayer = player1;
         }
     }
 
-    public void Start(){
+    public void Start()
+    {
 
-        while(true){
-        board.DisplayBoard();
-        Console.WriteLine($"Current player: {currentPlayer.PlayerType}");
-        Console.WriteLine("Enter row and column to place stone (e.g: O3:4) {O for ordinary Stone and H for Heavy Stone}:");
-        string? input=Console.ReadLine();
-
-
-        if (string.IsNullOrWhiteSpace(input))
+        while (true)
         {
-            Console.WriteLine("Invalid command.");
-            continue;
-        }
+            board.DisplayBoard();
+            Console.WriteLine($"Current player: {currentPlayer.PlayerType}");
+            Console.WriteLine("Enter row and column to place stone (e.g: O3:4) {O for ordinary Stone and H for Heavy Stone}:");
+            string? input = Console.ReadLine();
 
 
-        char command=input[0];
-        string coordinates=input.Substring(1);
-        string[] parts=coordinates.Split(":");
-
-        if (parts.Length != 2)
-        {
-            Console.WriteLine("Invalid command format.");
-            continue;
-        }
-
-        
-       if (!int.TryParse(parts[0], out int row) ||
-        !int.TryParse(parts[1], out int col))
-        {
-        Console.WriteLine("Invalid coordinates. Row and column must be numbers.");
-        continue;
-     }
-
-
-        if(command=='O' || command=='o'){
-            Stone stone=new Stone(StoneTypes.Ordinary,currentPlayer.PlayerType);
-            bool success=board.PlaceStone(row,col,stone);
-            if(success){
-                SwitchPlayer();
-            }
-        }
-        else if (command=='H' || command=='h'){
-            if(currentPlayer.HeavyStonesCount<=0){
-                Console.WriteLine($"No heavy stones left for {currentPlayer.PlayerType}");
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Invalid command.");
                 continue;
-            }else{
-                
-                Stone stone=new Stone(StoneTypes.Heavy,currentPlayer.PlayerType);
-                bool success=board.PlaceStone(row,col,stone);
-                if(success){
-                    currentPlayer.UseHeavyStone();
+            }
+
+
+            char command = input[0];
+            string coordinates = input.Substring(1);
+            string[] parts = coordinates.Split(":");
+
+            if (parts.Length != 2)
+            {
+                Console.WriteLine("Invalid command format.");
+                continue;
+            }
+
+
+            if (!int.TryParse(parts[0], out int row) ||
+             !int.TryParse(parts[1], out int col))
+            {
+                Console.WriteLine("Invalid coordinates. Row and column must be numbers.");
+                continue;
+            }
+
+
+            if (command == 'O' || command == 'o')
+            {
+                Stone stone = new Stone(StoneTypes.Ordinary, currentPlayer.PlayerType);
+                bool success = board.PlaceStone(row, col, stone);
+                if (success)
+                {
+                    if (board.CheckHorizontal(currentPlayer.PlayerType))
+                    {
+                        board.DisplayBoard();
+                        Console.WriteLine($"{currentPlayer.PlayerType} wins!");
+                        break;
+                    }
                     SwitchPlayer();
                 }
             }
-        }
-        else if(command=='E' || command=='e'){
-            if(currentPlayer.EraserCount<=0){
-                Console.WriteLine($"No erasers left for {currentPlayer.PlayerType}");
-                continue;
+            else if (command == 'H' || command == 'h')
+            {
+                if (currentPlayer.HeavyStonesCount <= 0)
+                {
+                    Console.WriteLine($"No heavy stones left for {currentPlayer.PlayerType}");
+                    continue;
+                }
+                else
+                {
+
+                    Stone stone = new Stone(StoneTypes.Heavy, currentPlayer.PlayerType);
+                    bool success = board.PlaceStone(row, col, stone);
+                    if (success)
+                    {
+                        if (board.CheckHorizontal(currentPlayer.PlayerType))
+                        {
+                            board.DisplayBoard();
+                            Console.WriteLine($"{currentPlayer.PlayerType} wins!");
+                            break;
+                        }
+                        currentPlayer.UseHeavyStone();
+                        SwitchPlayer();
+                    }
+                }
             }
-            bool success=board.EraseStone(row,col,currentPlayer.PlayerType);
-            if(success){
-                currentPlayer.UseEraser();
-                SwitchPlayer();
+            else if (command == 'E' || command == 'e')
+            {
+                if (currentPlayer.EraserCount <= 0)
+                {
+                    Console.WriteLine($"No erasers left for {currentPlayer.PlayerType}");
+                    continue;
+                }
+                bool success = board.EraseStone(row, col, currentPlayer.PlayerType);
+                if (success)
+                {
+
+                    currentPlayer.UseEraser();
+                    SwitchPlayer();
+                }
             }
-        }
-        else{
-            Console.WriteLine("Invalid command. Use 'O' for ordinary stone, 'H' for heavy stone, or 'E' for eraser.");
-        }
+            else
+            {
+                Console.WriteLine("Invalid command. Use 'O' = ordinary stone, 'H' = heavy stone, or 'E' = eraser.");
+            }
 
 
 
 
-        
+
         }
     }
 
